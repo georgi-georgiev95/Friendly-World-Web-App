ad7cd31fc3ae81159d5b665ead9db6b7d7cd6353
Georgi957 <gesh007@abv.bg>
2024-01-30 10:57:06 +0200

    Install mongoose and connect to mongodb

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const ENV = require('./utils/constants');
 const router = require('./router');
 
+// init
 const app = express();
 
+// setup DB
+mongoose.connect(ENV.DB_URI)
+    .then(() => console.log('Connected to DB...'))
+    .catch((err) => console.log(err));
+
+// router
 app.use(router);
 
 app.listen(ENV.PORT, () => console.log(`App is listening on port: ${ENV.PORT}`));
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,6 @@
 const ENV = {
     PORT: 3000,
+    DB_URI: 'mongodb://127.0.0.1:27017/friendlyWorld'
 };
 
 module.exports = ENV;
