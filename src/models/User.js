313e0e964262f6565527eb1f114a20cf4d0338c6
Georgi957 <gesh007@abv.bg>
2024-01-30 11:19:37 +0200

    Hash password before saving to DB

--- src/models/User.js
+++ src/models/User.js
@@ -12,6 +12,18 @@ const userSchema = new mongoose.Schema({
     }
 });
 
+userSchema.virtual('rePassword')
+    .set(function (value) {
+        if (value !== this.password) {
+            return mongoose.Error('Passwords must match!')
+        }
+    });
+
+userSchema.pre('save', async function () {
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
+})
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
