52af1f1b191157d5a1756b78570fb08cbfcda5e8
Georgi957 <gesh007@abv.bg>
2024-01-30 13:31:03 +0200

    fix owner id

--- src/controllers/animalController.js
+++ src/controllers/animalController.js
@@ -1,7 +1,19 @@
 const router = require('express').Router();
 
+const animalManager = require('../managers/animalManager');
+
 router.get('/create', (req, res) => {
     res.render('animals/create')
+});
+
+router.post('/create', async (req, res) => {
+    const animalData = {
+        ...req.body,
+        owner: req.user.id
+    };
+
+    await animalManager.create(animalData)
+    res.redirect('/');
 })
 
 module.exports = router;
