ab602edafde99ce1a336a0323c99b03c1770f421
Georgi957 <gesh007@abv.bg>
2024-01-30 11:54:22 +0200

    Login and register set cookie with the token

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 
 const userManager = require('../managers/userManager');
+const generateToken = require('../utils/generateToken');
+const ENV = require('../utils/constants');
+
 
 router.get('/register', (req, res) => {
     res.render('users/register');
@@ -10,11 +13,41 @@ router.get('/login', (req, res) => {
     res.render('users/login');
 });
 
+router.post('/login', async (req, res) => {
+    const userData = req.body;
+
+    try {
+        const user = await userManager.getOne(userData);
+        const token = await generateToken(user);
+
+        res.cookie(ENV.COOKIE_NAME, token);
+
+        res.redirect('/')
+    } catch (err) {
+        res.render('/users/login', { err });
+    }
+})
+
 router.post('/register', async (req, res) => {
     const userData = req.body;
-    await userManager.create(userData);
+    try {
+        const user = await userManager.create(userData);
+    
+        const token = await generateToken(user);
+    
+        res.cookie(ENV.COOKIE_NAME, token);
+    
+        res.redirect('/');
+    } catch (err) {
+        res.render('/users/register', {err});
+    }
+});
 
-    res.redirect('/');
+router.get('/logout', (req, res) => {
+    res.clearCookie(ENV.COOKIE_NAME);
+    res.redirect('/')
 })
+
+
 
 module.exports = router;
--- src/managers/userManager.js
+++ src/managers/userManager.js
@@ -1,3 +1,20 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
-exports.create = (userData) => User.create(userData); 
+exports.create = (userData) => User.create(userData);
+
+exports.getOne = async (userData) => {
+    const user = await User.find({ email: userData.email });
+
+    if (!user) {
+        throw new Error('Wrong username or password!');
+    }
+
+    const isMatch = await bcrypt.compare(userData.password, user.password);
+
+    if (!isMatch) {
+        throw new Error('Wrong username or password!');
+    }
+
+    return user;
+}
