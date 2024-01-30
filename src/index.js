fb95d5282175f4627101b42394be7f58459466ba
Georgi957 <gesh007@abv.bg>
2024-01-30 10:52:30 +0200

    Add router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const ENV = require('./utils/constants');
+const router = require('./router');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello from express!')
-});
+app.use(router);
 
 app.listen(ENV.PORT, () => console.log(`App is listening on port: ${ENV.PORT}`));
